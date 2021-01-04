32927bbb7f7bec58dd695a6d17ded94107c7fd69
plains1mple <up201806388@fe.up.pt>
2021-01-04 19:53:03 +0000

    probably done

--- TP2/proj2.pl
+++ TP2/proj2.pl
@@ -1,6 +1,69 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
+reset_timer :- statistics(walltime,_).	
+print_time :-
+	statistics(walltime,[_,T]),
+	TS is ((T//10)*10)/1000,
+	nl, write('Time: '), write(TS), write('s'), nl, nl.
+
+printBoard(Board):-
+    nl,
+    nth1(1, Board, Row),
+    length(Row, Length),
+    write('|'),
+    print_separator(0, Length),
+    nl,
+    printMatrix(Board, 0, Length).
+
+%prints the current game matrix in a board-like way
+printMatrix([], _, _).
+printMatrix([H|T], X, Length):-
+    write('|'),
+    X1 is X+1,
+    printLine(H),
+    nl,
+    write('|'),
+    print_separator(0, Length),
+    nl,
+    printMatrix(T, X1, Length).
+
+%prints a matrixs line
+printLine([]).
+printLine([H|T]):-
+    printElement(H),
+    printLine(T).
+
+printElement(H):-
+    H < 10,
+    write('    '),
+    write(H),
+    write(' |').
+printElement(H):-
+    H >= 10,
+    H < 100,
+    write('   '),
+    write(H),
+    write(' |').
+printElement(H):-
+    H >= 100,
+    H < 1000,
+    write('  '),
+    write(H),
+    write(' |').
+printElement(H):-
+    H >= 1000,
+    write(' '),
+    write(H),
+    write(' |').
+
+print_separator(Length, Length).
+print_separator(Aux, Length):-
+    write('------|'),
+    NewAux is Aux+1,
+    print_separator(NewAux, Length).
+
+
 flatten([], []) :- !. 
 flatten([L|Ls], FlatL) :-
     !,
@@ -8,6 +71,7 @@ flatten([L|Ls], FlatL) :-
     flatten(Ls, NewLs),
     append(NewL, NewLs, FlatL).
 flatten(L, [L]).
+
 
 unflatten([], _, _,_ , []).
 unflatten([H|T], N, N, CurrentRow, [NewCurrentRow|Rest]):-
@@ -18,24 +82,58 @@ unflatten([H|T], CurrentN, N, CurrentRow, Final):-
     NewCurrentN is CurrentN + 1,
     unflatten(T, NewCurrentN, N, NewCurrentRow, Final).
 
-constrainSum([], _).
+
+constrainSum([], _). %Solution must have sum equal to Value
 constrainSum([H|T], Value):-
     sum(H, #=, Value),
     constrainSum(T, Value).
 
-cnote(Input, Value, Output):-
-    flatten(Input, Vars), % Flatten the input, [[x,y,z], [x1,y1,z1], [x2,y2,z2]] = [x,y,z,x1,y1,z1,x2,y2,z2]
-    write('Input Vars: '), write(Vars), nl,
-    nth1(1, Input, Row), % Row = [x,y,z]
-    length(Row, N), % NxN board
-    Total is N*N,
-    write('Input N: '), write(N), nl, write('Input NxN: '), write(Total), nl,
+
+restriction(Var, Aux):- %Solution must contain digit Aux
+    Var mod 10 #= Aux.
+restriction(Var, Aux):-
+    Var #> 0,
+    Rest #= Var // 10,
+    restriction(Rest, Aux).
+
+addRestrictions([],_,_).
+addRestrictions([H|T], Vars, Index):-
+    nth1(Index, Vars, Aux), %Value of the input
+    restriction(H, Aux),
+    NewIndex is Index + 1,
+    addRestrictions(T, Vars, NewIndex).
+
+
+solution(Input, No, Value, Output):- %Board NxM ????? Only works on NxN boards right?
+    unflatten(Input, 1, No, [], Matrix),
+    nth1(1, Matrix, Col), % Row = [x,y,z]
+    length(Col, N),
+    Total is N*N, % NxN board
     length(Output, Total), % Output now has length Total
     domain(Output, 1, Value), % Values from Output in 1..100
     unflatten(Output, 1, N, [], Rows), % Reverse operation of flatten
-    write('Output Rows: '), write(Rows), nl,
     transpose(Rows, Cols), % Gives us Cols, since we need both Rows and Cols to be = Value 
-    write('Output Cols: '), write(Cols), nl,
-    constrainSum(Rows, Value), % Constrains the sum of both
-    constrainSum(Cols, Value), % Rows and Cols to Value
+    constrainSum(Rows, Value),
+    constrainSum(Cols, Value),
+    addRestrictions(Output, Input, 1),
     labeling([], Output).
+
+cnote(Input, No, Value):-
+    unflatten(Input, 1, No, [], Matrix),
+    nl, write('Matrix: '), nl,
+    printBoard(Matrix),
+    nth1(1, Matrix, Row),
+    length(Row, Length),
+    reset_timer, %start timer before solution
+    solution(Input, No, Value, Output),
+    unflatten(Output, 1, Length, [], Result),
+    write('Result: '), nl,
+    printBoard(Result),
+    print_time, nl,
+    fd_statistics.
+
+%cnoteGenerate(Length, Value, Output)
+
+%cnoteRandom(Length, Value, Output)
+
+%cnoteUnique(Length, Value, Output)
